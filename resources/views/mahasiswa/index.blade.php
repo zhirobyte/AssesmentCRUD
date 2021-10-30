8cc6535345e19d4b1669219eda22b800136ca8ac
zhirobyte <faizhanafi90@gmail.com>
2021-10-31 00:40:00 +0700

    rapikan table

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,20 +1,41 @@
 @extends('mahasiswa.layout')
 
 @section('content')
+<!-- ini adalah file cssnya untuk center table -->
+  
+<style type="text/css">
+    .table {
+        margin: 60px auto;
+        width: 85%;
+    }
+        h3 {
+    color:  white;
+    }
+        h5 {
+    color:  white;
+    }
+        
+ </style>
 
 
+    <h3 class="text-center">Assesment CRUD</h3>
+    <h5 class="text-center">Faiz Hanafi</h5>
+
+    <!-- 
+ini adalah line untuk table -->
  <table class="table table-bordered table-dark">
+ 
   <thead>
     <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Nama</th>
-      <th scope="col">Nim</th>
-      <th scope="col">Tgl Lahir</th>
-      <th scope="col">Alamat</th>
-      <th scope="col">Tahun Masuk</th>
-      <th scope="col">create at</th>
-      <th scope="col">update at</th>
-      <th scope="col"width="280px">Action</th>
+      <th scope="col"class="text-center">ID</th>
+      <th scope="col"class="text-center">Nama</th>
+      <th scope="col"class="text-center">Nim</th>
+      <th scope="col"class="text-center">Tgl Lahir</th>
+      <th scope="col"class="text-center">Alamat</th>
+      <th scope="col"class="text-center">Tahun Masuk</th>
+      <th scope="col"class="text-center">create at</th>
+      <th scope="col"class="text-center">update at</th>
+      <th scope="col"width="290px">Action</th>
     </tr>
   </thead>
   <tbody>
--- resources/views/mahasiswa/layout.blade.php
+++ resources/views/mahasiswa/layout.blade.php
@@ -11,8 +11,8 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1>Hello, Sukses selalu!</h1>
- 
+
+  <body style="background-color:#696969;">
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -21,5 +21,8 @@
   
   
     @yield('content')
+
+  
+
   </body>
 </html>
